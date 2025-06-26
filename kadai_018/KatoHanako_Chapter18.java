d1e79404d1bfaa575c8fddc7a17982f6a0bdda5e
lulepufuggler <misarinmii@gmail.com>
2025-06-27 03:23:36 +0900

    kadai_018をアップロード

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,16 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+	
+	public void setGivenName() {
+		familyName ="加藤";
+		givenName  ="花子";
+		address    ="東京都中野区〇×";
+		
+	}
+		
+		public void eachIntroduce() {
+	        System.out.println("趣味は読書です");
+	}
+
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,16 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+	
+	public void setGivenName() {
+		familyName ="加藤";
+		givenName  ="太郎";
+		address    ="東京都中野区〇×";
+		
+	}
+		
+		public void eachIntroduce() {
+	        System.out.println("私はJavaが得意です");
+	}
+
+}
